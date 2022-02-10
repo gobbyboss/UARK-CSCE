985ebf0c7792268f008ced30a6b7cf1ca5f14de7
gobbyboss <gobbyboss@gmail.com>
2022-02-09 20:56:51 -0600

    Databases Assignment 1 V2.0
    For part 2, added more features

--- Databases/Assignment1/DB.java
+++ Databases/Assignment1/DB.java
@@ -1,17 +1,21 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Scanner;
 import java.io.File;
 
 public class DB {
-  public static final int NUM_RECORDS = 10;
-  public static final int RECORD_SIZE = 71;
+  public static final int NUM_RECORDS = 1908;
+  public static final int RECORD_SIZE = 110;
+  public int tempRecordNum, tempId;
+  public String tempState, tempCity, tempName;
+  public Scanner inputScanner;
 
   private RandomAccessFile config, data, overflow;
   private int num_records, num_overflow;
-  private Boolean isOpen;
-  private int tempRecordNum, tempId;
-  private String tempState, tempCity, tempName; 
+  private Boolean isOpen, isOverflow;
+  private String dataFile, configFile, overflowFile;
+
 
 
   public DB() {
@@ -21,11 +25,16 @@ public class DB {
     this.num_records = 0;
     this.num_overflow = 0;
     this.isOpen = false;
+    this.isOverflow = false;
     this.tempRecordNum = -1;
     this.tempId = 0;
     this.tempState = null;
     this.tempCity = null;
     this.tempName = null;
+    this.dataFile = null;
+    this.configFile = null;
+    this.overflowFile = null;
+    inputScanner = new Scanner(System.in);
   }
 
 
@@ -46,9 +55,9 @@ public class DB {
       boolean fileExists = checkedFile.exists();
       if(!this.isOpen && fileExists)
       {
-        String dataFile = filename + ".data";
-        String configFile = filename + ".config";
-        String overflowFile = filename + ".overflow";
+        this.dataFile = filename + ".data";
+        this.configFile = filename + ".config";
+        this.overflowFile = filename + ".overflow";
         this.config = new RandomAccessFile(configFile, "rw");
         this.data = new RandomAccessFile(dataFile, "rw");
         this.overflow = new RandomAccessFile(overflowFile, "rw");
@@ -85,6 +94,9 @@ public class DB {
       this.isOpen = false;
       this.num_records = 0;
       this.num_overflow = 0;
+      this.dataFile = null;
+      this.configFile = null;
+      this.overflowFile = null;
     } catch (IOException e) {
       System.out.println("There was an error while attempting to close the database file.\n");
       e.printStackTrace();
@@ -97,7 +109,6 @@ public class DB {
   {
     return this.isOpen; 
   }
-
  
 
   /*
@@ -105,25 +116,23 @@ public class DB {
   Could read in the fields from the .config file instead of hardcoding, but instead decided on the faster route
   for this project.
   */
-  public boolean readRecord(int record_num) {
+  public boolean readRecord(int record_num, String filename) {
     
     String[] fields;
 
     if ((record_num >= 0) && (record_num < this.num_records)) {
       try {
-        data.seek(0); // return to the top of the file
-        data.skipBytes(record_num * RECORD_SIZE);
+        RandomAccessFile file = new RandomAccessFile(filename, "rw");
+        file.seek(0); // return to the top of the file
+        file.skipBytes(record_num * RECORD_SIZE);
         // parse record and update fields
-        fields = data.readLine().split(",",5); 
-        for(int i = 0; i < fields.length; i++)
-        {
-          System.out.println("Field " + i + ": " + fields[i]);
-        }
+        fields = file.readLine().split(",",6); 
         this.tempRecordNum = Integer.parseInt(fields[0]);
         this.tempId = Integer.parseInt(fields[1]);
         this.tempState = fields[2];
         this.tempCity = fields[3];
         this.tempName = fields[4];
+        file.close();
       } catch (IOException e) {
         System.out.println("There was an error while attempting to read a record from the database file.\n");
         e.printStackTrace();
@@ -134,12 +143,13 @@ public class DB {
     return true;
   }
 
+  //Writes a record to the end of the data file. Used for creating the DB
   public Boolean writeRecord(int recordNum, int id, String state, String city, String name)
   {
     if(this.isOpen = true)
     {
       byte[] bytesToWrite = new byte[RECORD_SIZE - 1];
-      String recordString = recordNum + "," + id + "," + state + "," + city + "," + name; 
+      String recordString = recordNum + "," + id + "," + state + "," + city + "," + name + ","; 
       try{
         byte[] recordBytes = recordString.getBytes("UTF8");
         for(int i = 0; i < RECORD_SIZE; i++)
@@ -170,6 +180,165 @@ public class DB {
     }
   }
 
+  //Calls binary search on the data file, and if not found sequentially looks through the overflow file
+  public Boolean findRecord(int id)
+  {
+    int recordId = binarySearch(id, this.dataFile);
+    this.isOverflow = false;
+    if(recordId == -1)
+    {
+      byte[] isOverflowEmpty = this.overflowFile.getBytes();
+      //Checking if overflow is empty before searching, the number of bytes of an empty file is 20
+      if(isOverflowEmpty.length == 20)
+      {
+        System.out.println("\nRecord was not found.\n");
+        return false;
+      }
+      boolean found = false;
+      if(num_overflow != 0)
+      {
+        for(int i = 0; i < num_overflow; i++)
+        {
+          readRecord(i, overflowFile);
+          int idFound = id - this.tempId;
+          if(idFound == 0)
+          {
+            recordId = i;
+            this.isOverflow = true;
+            found = true;
+            break;
+          }
+        }
+        if(!found)
+        {
+          System.out.println("\nRecord was not found.\n");
+          return false;
+        }
+      }
+      else
+      {
+        System.out.println("\nRecord was not found.\n");
+        return false;
+      }
+    }
+    boolean recordRead = readRecord(recordId, this.dataFile);
+    if(recordRead)
+    {
+      System.out.println("\nData for Record #" + recordId + "\n-------------------\n" +
+      "ID: " + this.tempId + "\nState: " + this.tempState + "\nCity: " + this.tempCity + "\nName: " + this.tempName + "\n");
+      return true;
+    }
+    else
+    {
+      System.out.println("Record could not be found.\n");
+      return false;
+    }
+  }
+
+  //Prompts the user for the ID of a record to search, and what field they want updated. Then calls overwriteRecord
+  public boolean updateRecord()
+  {
+    if(!this.isOpen)
+    {
+      System.out.println("\nA database is not open");
+      return false;
+    }
+    System.out.print("Please input the ID of the record you would like to update: ");
+    String updateInput = inputScanner.nextLine();
+    int updateId = 0;
+    try{
+      updateId = Integer.parseInt(updateInput);
+    }catch(NumberFormatException e){
+      System.out.println("Sorry that is not a valid input\n");
+      return false;
+    }
+    boolean recordFound = findRecord(updateId);
+    if(!recordFound)
+    {
+      return false;
+    }
+    System.out.print("Update options\n---------------\n1) State \n2) City \n3) Name \n4) Exit\n\nEnter the integer"
+    + " of the field you want to update: ");
+    updateInput = inputScanner.nextLine();
+    try{
+      updateId = Integer.parseInt(updateInput);
+    }catch(NumberFormatException e){
+      System.out.println("Sorry that is not a valid input\n");
+      return false;
+    }
+    switch(updateId)
+    {
+      case 1:
+        System.out.print("\n\"" + this.tempState + "\" will be updated. Please enter the new State here: ");
+        updateInput = inputScanner.nextLine();
+        overwriteRecord(updateInput, this.tempCity, this.tempName);
+        break;
+      case 2:
+        System.out.print("\n\"" + this.tempCity + "\" will be updated. Please enter the new City here: ");
+        updateInput = inputScanner.nextLine();
+        overwriteRecord(this.tempState, updateInput, this.tempName);
+        break;
+      case 3:
+        System.out.print("\n\"" + this.tempName + "\" will be updated. Please enter the new name here: ");
+        updateInput = inputScanner.nextLine();
+        overwriteRecord(this.tempState, this.tempCity, updateInput);
+        break;
+      case 4:
+        System.out.println("No fields were updated.");
+        return false;
+      default:
+        System.out.println("That input is not valid.");
+        return false;
+    }
+    return true;
+  }
+
+  //Checks if its overwriting overflow or data file, then writes a new record at the old record location
+  public Boolean overwriteRecord(String state, String city, String name)
+  {
+    if(!this.isOpen)
+    {
+      return false;
+    }
+    RandomAccessFile file = data;
+    if(this.isOverflow)
+    {
+      file = overflow;
+    }
+    try{
+      file.seek((this.tempRecordNum - 1) * RECORD_SIZE);
+    }catch(IOException e)
+    {
+      System.out.println(e.toString());
+    }
+  
+    byte[] bytesToWrite = new byte[RECORD_SIZE - 1];
+    String recordString = this.tempRecordNum + "," + this.tempId + "," + state + "," + city + "," + name + ","; 
+    try{
+      byte[] recordBytes = recordString.getBytes("UTF8");
+      for(int i = 0; i < RECORD_SIZE; i++)
+      {
+        if(i < recordBytes.length)
+        {
+          bytesToWrite[i] = recordBytes[i];
+        }
+        if(i >= recordBytes.length && i < RECORD_SIZE - 1)
+        {
+          bytesToWrite[i] = ' ';
+        }
+      }
+      String newline = "\n";
+      byte[] newlineBytes = newline.getBytes();
+      file.write(bytesToWrite);
+      file.write(newlineBytes);
+    }
+    catch(IOException e)
+    {
+      System.out.print(e.toString());
+    }
+    return true;
+  }
+
   /**
    * Binary Search by record id
    * 
@@ -177,7 +346,7 @@ public class DB {
    * @return Record number (which can then be used by read to
    *         get the fields) or -1 if id not found
    */
-  public int binarySearch(int id) {
+  public int binarySearch(int id, String filename) {
     int Low = 0;
     int High = NUM_RECORDS - 1;
     int Middle = 0;
@@ -186,7 +355,7 @@ public class DB {
 
     while (!Found && (High >= Low)) {
       Middle = (Low + High) / 2;
-      readRecord(Middle);
+      readRecord(Middle, filename);
 
       // int result = MiddleId[0].compareTo(id); // DOES STRING COMPARE
       int result = this.tempId - id; // DOES INT COMPARE of MiddleId[0] and id
