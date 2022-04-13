681c750b862e26637df6c0ac16680a21dda8bb49
gobbyboss <gobbyboss@gmail.com>
2022-04-12 20:34:12 -0500

    Networks Hw7 Part 2

--- Computer Networks/hw7/server.py
+++ Computer Networks/hw7/server.py
@@ -1,32 +1,32 @@
+from socket import socket
 import socketserver
 
-class Server():
-    def __init__(self, data):
-        self.isRunning = True
-        self.data = data
-        try:
-            self.balanceFile = open("balance.txt", "r+")
-            self.balanceFile.close()
-            print('Server is ready.')
-        except IOError:
-            self.balance = 100
-            print('The balance has been initialized to 100')
-
-  
 class TCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        s = Server(self.data)
+        self.server.execute(format(self.data))
         print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
         # just send back the same data, but upper-cased
         self.request.sendall(self.data.upper())
-    
 
-      
+class Server(socketserver.TCPServer):
+    def __init__(self, server_address, RequestHandlerClass):
+        socketserver.TCPServer.__init__(self, server_address, RequestHandlerClass, bind_and_activate=True)
+        self.isRunning = True
+        try:
+            self.balanceFile = open("balance.txt", "r+")
+            self.balanceFile.close()
+        except IOError:
+            self.balance = 100
+            
+        print('Welcome to your bank account!\n===================================\n')
+
+    def execute(self, data):
+        print('Please select an option.')
+   
 
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
-    with socketserver.TCPServer((HOST, PORT), TCPHandler) as server:
+    with Server((HOST, PORT), TCPHandler) as server:
         server.serve_forever()
