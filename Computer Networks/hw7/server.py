99e4e38a56f56310603a92095e0b95a760f352f6
gobbyboss <gobbyboss@gmail.com>
2022-04-13 17:43:28 -0500

    Networks Hw7 Part 3

--- Computer Networks/hw7/server.py
+++ Computer Networks/hw7/server.py
@@ -4,29 +4,31 @@ import socketserver
 class TCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        self.server.execute(format(self.data))
+        self.dataToSend = self.server.execute(format(self.data))
         print("{} wrote:".format(self.client_address[0]))
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+        self.request.sendall(bytes(self.dataToSend + "\n", "utf-8"))
 
 class Server(socketserver.TCPServer):
     def __init__(self, server_address, RequestHandlerClass):
         socketserver.TCPServer.__init__(self, server_address, RequestHandlerClass, bind_and_activate=True)
-        self.isRunning = True
+        self.firstRequest = True
         try:
             self.balanceFile = open("balance.txt", "r+")
             self.balanceFile.close()
         except IOError:
             self.balance = 100
             
-        print('Welcome to your bank account!\n===================================\n')
-
+        
     def execute(self, data):
-        print('Please select an option.')
-   
-
-
+        if self.firstRequest:
+            self.firstRequest = False
+            return ('Welcome to your bank account!\n===================================\nPlease select an option.'
+            + '\n1) Check your balance\n2) Select an input\n')
+    
+        
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
+    print('Server starting...')
     with Server((HOST, PORT), TCPHandler) as server:
         server.serve_forever()
+    print('Server exiting...')
